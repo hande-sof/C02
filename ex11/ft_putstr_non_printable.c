d8bcc479cad03f2a395479ff2daff5aa705a66fd
Soim Lee <soilee@c6r10s3.42seoul.kr>
2020-07-13 22:15:24 +0900

    ex06

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -1,25 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/13 19:06:24 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/13 21:26:19 by soilee           ###   ########.fr       */
+/*   Created: 2020/07/13 21:22:18 by soilee            #+#    #+#             */
+/*   Updated: 2020/07/13 22:15:03 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_printable(char *str)
+#include <unistd.h>
+
+void	print_hex(char c)
+{
+	write(1, "\\", 1);
+}
+
+void	ft_putstr_non_printable(char *str)
 {
 	int i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] < 32) || (str[i] > 127))
-			return (0);
-		i++
+		if (str[i] < 32 && str[i] > 127)
+			write(1, &str[i], 1);
+		else
+			print_hex(str[i]);
 	}
-	return (1);
 }
