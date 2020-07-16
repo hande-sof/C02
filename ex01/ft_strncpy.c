35cc532ec79d6a40b4a25eb6534907a16ff9950b
soilee <hande.sof@gmail.com>
2020-07-16 16:55:03 +0900

    ex01,10 m

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,19 +6,24 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 19:47:02 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/15 20:42:31 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/16 16:51:13 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int	i;
+	int				null_check;
 
 	i = 0;
+	null_check = 0;
 	while (i < n)
 	{
-		if (src[i] == '\0')
+		if (src[i] == '\0' || null_check == 1)
+		{
 			dest[i] = '\0';
+			null_check = 1;
+		}
 		else
 			dest[i] = src[i];
 		i++;
--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 20:34:18 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/14 21:11:06 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/16 16:54:23 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,15 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 
 	i = 0;
 	int_size = (int)size - 1;
-	while (i <= int_size && src[i] != '\0')
+	while (i <= int_size)
 	{
 		if (i < int_size)
 			dest[i] = src[i];
-		else if (i == int_size)
+		else if (i == int_size || src[i] == '\0')
 			dest[i] = '\0';
 		i++;
 	}
+	i = 0;
 	while (src[i] != '\0')
 		i++;
 	return (i);
